521ca65441075b1330950bc3784217ef4d0452b1
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-17 07:32:23 +0900

    Add basic coffee-making process to main

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,5 +1,13 @@
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        String coffeeProcess = """
+                Starting to make a coffee
+                Grinding coffee beans
+                Boiling water
+                Mixing boiled water with crushed coffee beans
+                Pouring coffee into the cup
+                Pouring some milk into the cup
+                Coffee is ready!""";
+        System.out.println(coffeeProcess);
     }
 }

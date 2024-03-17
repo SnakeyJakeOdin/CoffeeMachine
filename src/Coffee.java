da89dca59c6ab3c845fc46103258801537326f15
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-17 09:41:35 +0900

    Create coffee class

---
+++ src/Coffee.java
@@ -0,0 +1,11 @@
+public class Coffee {
+    private int numCups;
+
+    public Coffee(int numCups) {
+        this.numCups = numCups;
+    }
+
+    public int getNumCups() {
+        return this.numCups;
+    }
+}

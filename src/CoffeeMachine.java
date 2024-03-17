2f41595734951fc7f611d617602cb5f849842e98
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-17 10:00:01 +0900

    Create getNumCups() to pass onto Coffee object

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
+
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // Create Coffee object
-        Coffee coffee = new Coffee(5);
+        Coffee coffee = new Coffee(getNumCups());
 
         coffee.printIngredients();
     }
 
+    public static int getNumCups() {
+        String s = "Write how many cups of coffee you will need:";
+        System.out.println(s);
+
+        int numCups = 0;
+        while (true) {
+            try {
+                numCups = Integer.parseInt(scanner.nextLine());
+            } catch (Exception e) {
+                System.out.println("ERROR: Please enter a number");
+                continue;
+            }
+            if (numCups < 0) {
+                System.out.println("ERROR: Number cannot be negative");
+            }
+            else if (numCups == 0) {
+                System.out.println("ERROR: Number cannot be 0");
+            }
+            else {
+                break;
+            }
+        }
+
+        return numCups;
+    }
     public static void coffeeProcess() {
         String s = """
                 Starting to make a coffee

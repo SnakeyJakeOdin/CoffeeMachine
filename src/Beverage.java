32eb11df01666d999244f8886c44e982f006d605
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-18 15:07:15 +0900

    Update files to support Coffee parent class

---
+++ src/Beverage.java
@@ -0,0 +1,58 @@
+public class Beverage extends Coffee {
+    private int numCups;
+    public int cost;
+
+    public Beverage(int water, int milk, int beans, int cost) {
+        super(water, milk, beans);
+        this.cost = cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String toString() {
+        String s = String.format("""
+                Cost: $%d
+                """, cost);
+        return super.toString() + s;
+    }
+
+    public int checkSupplies() {
+        final int waterPerCup = 200; // ml
+        final int milkPerCup = 50;   // ml
+        final int beansPerCup = 15;  // g
+
+        int waterRatio = water / waterPerCup;
+        int milkRatio = milk / milkPerCup;
+        int beanRatio = beans / beansPerCup;
+
+        // water limiting factor
+        int limitingFactor;
+        if (waterRatio <= milkRatio && waterRatio <= beanRatio) {
+            limitingFactor = waterRatio;
+        }
+        // milk limiting factor
+        else if (milkRatio <= waterRatio &&  milkRatio <= beanRatio) {
+            limitingFactor = milkRatio;
+        }
+        // bean limiting factor
+        else {
+            limitingFactor = beanRatio;
+        }
+        return limitingFactor;
+    }
+
+    public void printAvailability (int limitingFactor) {
+        if (limitingFactor == numCups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        else if (limitingFactor > numCups) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)",
+                    limitingFactor - numCups);
+        }
+        else {
+            System.out.printf("No, I can only make %d cup(s) of coffee", limitingFactor);
+        }
+    }
+}
--- src/Coffee.java
+++ src/Coffee.java
@@ -1,5 +1,4 @@
 public class Coffee {
-    private int numCups;
     public int water;
     public int milk;
     public int beans;
@@ -10,8 +9,13 @@ public class Coffee {
         this.beans = beans;
     }
 
-    public void setNumCups(int numCups) {
-        this.numCups = numCups;
+    public String toString() {
+        String s = String.format("""
+                Water: %d ml
+                Milk: %d ml
+                Beans: %d g
+                """, water, milk, beans);
+        return s;
     }
     public void setWater(int water) {
         this.water = water;
@@ -21,51 +25,5 @@ public class Coffee {
     }
     public void setBeans(int beans) {
         this.beans = beans;
-    }
-    public String toString() {
-        String s = String.format("""
-                Water: %d ml
-                Milk: %d ml
-                Beans: %d g
-                """, water, milk, beans);
-        return s;
-    }
-
-    public int checkSupplies() {
-        final int waterPerCup = 200; // ml
-        final int milkPerCup = 50;   // ml
-        final int beansPerCup = 15;  // g
-
-        int waterRatio = water / waterPerCup;
-        int milkRatio = milk / milkPerCup;
-        int beanRatio = beans / beansPerCup;
-
-        // water limiting factor
-        int limitingFactor;
-        if (waterRatio <= milkRatio && waterRatio <= beanRatio) {
-            limitingFactor = waterRatio;
-        }
-        // milk limiting factor
-        else if (milkRatio <= waterRatio &&  milkRatio <= beanRatio) {
-            limitingFactor = milkRatio;
-        }
-        // bean limiting factor
-        else {
-            limitingFactor = beanRatio;
-        }
-        return limitingFactor;
-    }
-
-    public void printAvailability (int limitingFactor) {
-        if (limitingFactor == numCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        }
-        else if (limitingFactor > numCups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)",
-                    limitingFactor - numCups);
-        }
-        else {
-            System.out.printf("No, I can only make %d cup(s) of coffee", limitingFactor);
-        }
     }
 }

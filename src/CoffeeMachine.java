eea3cf13d868651f96eeb22d4a8d4177a5c12f72
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-19 16:09:52 +0900

    Add method buyCoffee
    This method asks the client which coffee they would like to buy. It then takes that input and passes it to updateMachine were the ingredients for the bought coffee are subtracted from the machine.

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -19,40 +19,36 @@ public class CoffeeMachine {
         System.out.println("\nWrite action (buy, fill, take):");
         s = scanner.nextLine();
 
-        if (s.equals("buy")) {
-            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-            n = scanner.nextInt();
-            switch (n) {
-                case 1:
-                    updateMachine(coffeeMachine, espresso);
-                    break;
-                case 2:
-                    updateMachine(coffeeMachine, latte);
-                    break;
-                case 3:
-                    updateMachine(coffeeMachine, cappuccino);
-                    break;
-                default:
-                    break;
-            }
+        if ("buy".equals(s)) {
+            buyCoffee(coffeeMachine, espresso, latte, cappuccino);
         }
-        else if (s.equals("fill")) {
-            System.out.println("Write how many ml of water you want to add:");
-            coffeeMachine.setWater(coffeeMachine.getWater() + scanner.nextInt());
-            System.out.println("Write how many ml of milk you want to add:");
-            coffeeMachine.setMilk(coffeeMachine.getMilk() + scanner.nextInt());
-            System.out.println("Write how many grams of coffee beans you want to add:");
-            coffeeMachine.setBeans(coffeeMachine.getBeans() + scanner.nextInt());
-            System.out.println("Write how many disposable cups you want to add:");
-            coffeeMachine.setCups(coffeeMachine.getCups() + scanner.nextInt());
-            System.out.println();
+        else if ("fill".equals(s)) {
+            fillMachine(coffeeMachine);
         }
-        else if (s.equals("take")) {
+        else if ("take".equals(s)) {
             System.out.printf("I gave you $%d%n%n", coffeeMachine.getMoney());
             coffeeMachine.setMoney(0);
         }
 
         System.out.println(coffeeMachine);
+    }
+
+    public static void buyCoffee(Machine coffeeMachine, Beverage espresso, Beverage latte, Beverage cappuccino) {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        int n = scanner.nextInt();
+        switch (n) {
+            case 1:
+                updateMachine(coffeeMachine, espresso);
+                break;
+            case 2:
+                updateMachine(coffeeMachine, latte);
+                break;
+            case 3:
+                updateMachine(coffeeMachine, cappuccino);
+                break;
+            default:
+                break;
+        }
     }
 
     public static void updateMachine(Machine coffeeMachine, Beverage beverage) {
@@ -61,6 +57,18 @@ public class CoffeeMachine {
         coffeeMachine.setBeans(coffeeMachine.getBeans() - beverage.getBeans());
         coffeeMachine.setCups(coffeeMachine.getCups() - 1);
         coffeeMachine.setMoney(coffeeMachine.getMoney() + beverage.getCost());
+        System.out.println();
+    }
+
+    public static void fillMachine(Machine coffeeMachine) {
+        System.out.println("Write how many ml of water you want to add:");
+        coffeeMachine.setWater(coffeeMachine.getWater() + scanner.nextInt());
+        System.out.println("Write how many ml of milk you want to add:");
+        coffeeMachine.setMilk(coffeeMachine.getMilk() + scanner.nextInt());
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        coffeeMachine.setBeans(coffeeMachine.getBeans() + scanner.nextInt());
+        System.out.println("Write how many disposable cups you want to add:");
+        coffeeMachine.setCups(coffeeMachine.getCups() + scanner.nextInt());
         System.out.println();
     }
 }
